0c1ea7d1fdd41a25cdbbb924a87cf5cf547ef1f6
Jonathan Ho <jona.tuan.h@gmail.com>
2023-01-02 23:05:21 -0800

    created basic app layout and began coding.

--- app/src/main/java/com/example/workoutformatter/MainActivity.kt
+++ app/src/main/java/com/example/workoutformatter/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.workoutformatter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pullUpInput = findViewById<EditText>(R.id.etNumPullUp)
+        val resultField = findViewById<EditText>(R.id.etResult)
+        var resultFieldText = resultField.text
+        val numPullUps = pullUpInput.text.toString()
+
+        var result = "$numPullUps pull-ups (/5)"
+        resultFieldText = result
     }
 }
