2ac6cb85199a3a5535dc16d12716e98e88ae6735
Jonathan Ho <jona.tuan.h@gmail.com>
2023-01-03 23:30:37 -0800

    Succesfully formatted one exercise and began working on easy-copy button

--- app/src/main/java/com/example/workoutformatter/MainActivity.kt
+++ app/src/main/java/com/example/workoutformatter/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.workoutformatter
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
@@ -11,11 +12,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val pullUpInput = findViewById<EditText>(R.id.etNumPullUp)
-        val resultField = findViewById<EditText>(R.id.etResult)
-        val numPullUps = pullUpInput.text.toString()
-        val result = "$numPullUps pull-ups (/5)"
+        val formatButton = findViewById<Button>(R.id.btnFormat)
+        val intent = Intent(this, WorkingSecondActivity::class.java)
 
-        val intent = Intent(this, SecondActivity::class.java)
-        resultField.setText(result)
+        formatButton.setOnClickListener {
+            val numPullUps = pullUpInput.text.toString()
+            intent.putExtra("NUM_PULL_UPS", numPullUps)
+            startActivity(intent)
+        }
     }
 }
