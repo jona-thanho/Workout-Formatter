7b2a5d20b7bc077493bc55091f06233923b39b65
Jonathan Ho <jona.tuan.h@gmail.com>
2023-01-04 22:59:57 -0800

    Implemented instant-copy button

--- app/src/main/java/com/example/workoutformatter/FourthActivity.kt
+++ app/src/main/java/com/example/workoutformatter/FourthActivity.kt
@@ -1,6 +1,5 @@
 package com.example.workoutformatter
 
-import android.R.attr.label
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
@@ -9,27 +8,29 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
-
-class WorkingSecondActivity : AppCompatActivity() {
+class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second_working)
+        setContentView(R.layout.activity_third)
         // display input into copyable format
         val resultField = findViewById<EditText>(R.id.etResult)
         val numPullUps = intent.getStringExtra("NUM_PULL_UPS")
-        val result = "$numPullUps pull-ups (/5)\n" +
-                "7, 6 diamond push-up (4/5)\n" +
-                "5, 4 chin up (2/5)\n" +
-                "21, 8 standard push-up (2/5)"
+        val numDmdPushUps = intent.getStringExtra("NUM_DMD_PUSH_UPS")
 
+        val result = "$numPullUps pull-ups (/5)\n" +
+                "$numDmdPushUps diamond push-up (4/5)\n" +
+                "??? chin up (2/5)\n" +
+                "??? standard push-up (2/5)"
         resultField.setText(result)
 
         // create button for instant copying
+
         val copyButton = findViewById<Button>(R.id.btnCopy)
+        val resultFieldText = resultField.text.toString()
         copyButton.setOnClickListener {
             val clipboard =
                 getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-            val clip = ClipData.newPlainText(label, text) as ClipData
+            val clip = ClipData.newPlainText(resultFieldText, resultFieldText) as ClipData
             clipboard.setPrimaryClip(clip)
         }
     }
